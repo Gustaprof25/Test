475a47bc110f410e125ef2fa78e1ea112bedd440
Gustaprof25 <gustaprof25@gmail.com>
2021-04-19 00:32:13 -0300

    Create MathUtil.java

---
+++ src/main/java/MathUtil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class MathUtil {
+    public static int mdc(int a, int b ){
+        if(b > 0 && a % b == 0){
+            return b;
+        }
+        
+        
+        return -1;
+    }
+}
